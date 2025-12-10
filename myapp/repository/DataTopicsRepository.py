f6d3da4fc869b2b09daf1c1bdd507b6d3a63bf88
BVASQUEZ\bvasq <bvasquez>
2025-12-10 11:13:49 -0500

    Solo usar activos

--- myapp/repository/DataTopicsRepository.py
+++ myapp/repository/DataTopicsRepository.py
@@ -5,12 +5,12 @@ import sys
 class DataTopicsRepository:
     @staticmethod
     def get_all_courses() -> list[str]:
-        sql = text("SELECT DISTINCT Course FROM data_topics")
+        sql = text("SELECT DISTINCT Course FROM data_topics where Status = 'A'")
         with db.engine.connect() as conn:
             return [row["Course"] for row in conn.execute(sql).mappings().all()]
 
     @staticmethod
     def get_topic_ids_by_course(course: str) -> list[int]:
-        sql = text("SELECT TopicID FROM data_topics WHERE Course = :course")
+        sql = text("SELECT TopicID FROM data_topics WHERE Course = :course and Status = 'A'")
         with db.engine.connect() as conn:
             return [row["TopicID"] for row in conn.execute(sql, {"course": course}).mappings().all()]
